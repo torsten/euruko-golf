b4a4f5aa72550f09366e013406dab042131a429e
Hans Hasselberg <me@hans.io>
2012-05-06 11:29:24 +0200

    Improve readability.

--- i0rek.rb
+++ i0rek.rb
@@ -1,8 +1,8 @@
 eval"
 
-'ue R E UU  e e e w  W
-R   ' . u n p a c k ( '
-B2  4 ' *4  ) . ma  p {
-|a| p=a . t r'0 1 '  ,
++ue R E UU  e e e w  W
+R   + M u n p a c k ( +
+B2  4 + *4  ) M ma  p {
+XaX p=a M t r+0 1 +  ,
 
-'=#'}".tr("\s\n","").tr("="," ")
+'=#'}".tr("\s\n","").tr("=+MX"," '.|")
