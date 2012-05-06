f8032f81b70aa9e0d3f717cd8f7e721537dcae45
Hans Hasselberg <me@hans.io>
2012-05-06 11:24:04 +0200

    Golf down size.

--- i0rek.rb
+++ i0rek.rb
@@ -1,8 +1,8 @@
-eval %Q{
+eval"
 
 'ue R E UU  e e e w  W
 R   ' . u n p a c k ( '
 B2  4 ' *4  ) . ma  p {
 |a| p=a . t r'0 1 '  ,
 
-'=#'}}.tr("\s\n","").tr("="," ")
+'=#'}".tr("\s\n","").tr("="," ")
